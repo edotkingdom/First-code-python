51708a413c9203584beb1428d620456713f1fd6d
redhat <redhanoormansyah30@gmail.com>
2021-11-07 23:40:44 +0700

    IF else

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 """
 Ini adalah project Pertama dengan python
+Semua sintaksi dasar bhs. Pemprograman terdiri dari
+1. Sintaksis    : Langkah Pertama
+2. Pecabangan   : Langkah jika kondisi terpenuhi
+3. Perulangan   : Langkah mengulang berkali - kali sampai kondisi terpenuhi
 """
-print("Heloo World")
-print("Heloo World")
+# Sekunsial
+
+print('Ibu berkata," Pergi Ke Toko Bud"')
+print('budi Menjawab," Baik, apa yang harus saya lakukan di toko"')
+print('Ibu Menjawab," Beli satu susu botol, dan jika ada telor beli 6"')
+print("Maka budi berangkat ke toko")
+print("dan mulai belanja")
+
+# Percabangan
+jumlah_botol_susu = 177
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harga susu, ternyata cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi beli susu")
+
+print("Budi Pulang kerumah")
+print("Menyampaikan hasilnya ke ibu")
+
