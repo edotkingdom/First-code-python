0570c1075244b6a4593a83f7b89adb558eee34b5
redhat <redhanoormansyah30@gmail.com>
2021-11-08 21:48:14 +0700

    Add Perulangan

---
+++ dasar_perulangan.py
@@ -0,0 +1,7 @@
+jumlah_buku = 100
+print("Baca Semua buku")
+jumlah_buku_telah_dibaca = 0
+print(f"jumlah buku yang dibaca adalah {jumlah_buku_telah_dibaca}")
+
+for jumlah_buku_telah_dibaca in range(1,jumlah_buku+1):
+    print(f"buku ke -{jumlah_buku_telah_dibaca}")
