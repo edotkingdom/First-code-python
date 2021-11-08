3fae4de854baf7583f33f18864ac3db200fb603f
redhat <redhanoormansyah30@gmail.com>
2021-11-08 22:00:29 +0700

    Add Perulangan For & While

--- dasar_perulangan.py
+++ dasar_perulangan.py
@@ -1,7 +1,19 @@
-jumlah_buku = 100
+# Bentuk For
+
+jumlah_buku = 10
 print("Baca Semua buku")
 jumlah_buku_telah_dibaca = 0
 print(f"jumlah buku yang dibaca adalah {jumlah_buku_telah_dibaca}")
 
 for jumlah_buku_telah_dibaca in range(1,jumlah_buku+1):
     print(f"buku ke -{jumlah_buku_telah_dibaca}")
+
+# Bentuk While
+print("+++Bentuk while++++")
+jumlah_buku_while = 100
+jumlah_buku_telah_dibaca_while =0
+while jumlah_buku_telah_dibaca_while < jumlah_buku_while:
+    jumlah_buku_telah_dibaca_while = jumlah_buku_telah_dibaca_while+1
+    print(f"jumlah buku yang telah dibaca {jumlah_buku_telah_dibaca_while}")
+
+print(f"jumlah buku yang telah dibaca{jumlah_buku_telah_dibaca_while}")
